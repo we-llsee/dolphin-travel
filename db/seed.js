82c1243dd4957bf061650693e39cd21071dccfcf
JessalynKemp <jessalyn1@btinternet.com>
2022-08-10 12:35:23 +0100

    make test look for .env files conditionally

--- db/seed.js
+++ db/seed.js
@@ -4,9 +4,11 @@ const ENV = process.env.NODE_ENV || "test";
 
 const { MongoClient } = require("mongodb");
 
-require("dotenv").config({
-  path: `${__dirname}/../${ENV}.env`,
-});
+if (ENV !== "production") {
+  require("dotenv").config({
+    path: `${__dirname}/../${ENV}.env`,
+  });
+}
 
 const MONGO_PW = process.env.MONGO_PW;
 const MONGO_USER = process.env.MONGO_USER;
