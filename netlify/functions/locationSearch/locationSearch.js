6249710ebe6934816a899f9df8ad0e89074d94bd
will <will@wills-MacBook-Pro.local>
2022-08-16 15:43:43 +0100

    final draft v1. Commiting to test when hosted on netlify

--- netlify/functions/locationSearch/locationSearch.js
+++ netlify/functions/locationSearch/locationSearch.js
@@ -2,28 +2,25 @@ const axios = require("axios");
 
 const handler = async function (event) {
   try {
+    let { q } = event.queryStringParameters;
+    let { countrycodes } = event.queryStringParameters;
 
-    let {large}= event.queryStringParameters;
-    console.log(large);
-    console.log(event.queryStringParameters)
-    
     const response = await axios.get(
-      `https://eu1.locationiq.com/v1/search?key=pk.925883abdd6280b4428e57337de16f23&q=hilton&addressdetails=1&countrycodes=fr&format=json`
+      `https://eu1.locationiq.com/v1/search?key=pk.925883abdd6280b4428e57337de16f23&q=${q}&addressdetails=1&countrycodes=${countrycodes}&format=json&normalizeaddress=1`
     );
-    if (response.status<200 || response.status>=300) {
-      return { statusCode: response.status, body: response.statusText }
+    if (response.status < 200 || response.status >= 300) {
+      return { statusCode: response.status, body: response.statusText };
     }
 
     return {
       statusCode: 200,
       body: JSON.stringify({ msg: response.data }),
-    }
-
+    };
   } catch (error) {
     console.log(error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg:"Server Error" }),
+      body: JSON.stringify({ msg: "Server Error" }),
     };
   }
 };
