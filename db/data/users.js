ca7e55ca7f948a8e70b2a2ffa466552774ad82f8
we-llsee <67220039+we-llsee@users.noreply.github.com>
2022-08-17 09:42:32 +0100

    Update users.js
    Link to images hosted on Github (to (easily) host images on Netlify you need to directly reference them from your frontend code)

--- db/data/users.js
+++ db/data/users.js
@@ -3,24 +3,24 @@ module.exports = [
     firstName: "Will",
     lastName: "Clegg",
     _id: "willclegg",
-    avatarURL:"./assets/willAv.jpg"
+    avatarURL:"https://github.com/we-llsee/dolphin-travel/blob/frontend/src/assets/images/willAv.jpg?raw=true"
   },
   {
     firstName: "Jess",
     lastName: "Kemp",
     _id: "jesskemp",
-    avatarURL:"./assets/jessAv.jpg"
+    avatarURL:"https://github.com/we-llsee/dolphin-travel/blob/frontend/src/assets/images/jessAv.jpg?raw=true"
   },
   {
     firstName: "Alex",
     lastName: "Rong",
     _id: "alexrong",
-    avatarURL:"./assets/alexAv.jpg"
+    avatarURL:"https://github.com/we-llsee/dolphin-travel/blob/frontend/src/assets/images/alexAv.jpg?raw=true"
   },
   {
     firstName: "Mohamed",
     lastName: "Elrofai",
     _id: "mohamedelrofai",
-    avatarURL:"./assets/mohamedAv.jpg"
+    avatarURL:"https://github.com/we-llsee/dolphin-travel/blob/frontend/src/assets/images/mohamedAv.jpg?raw=true"
   },
 ];
