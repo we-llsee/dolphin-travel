ec3f53730e686d0495562a8c5a1b099b2a41e498
will <will@wills-MacBook-Pro.local>
2022-08-17 10:30:36 +0100

    Update components to deal with the response structure received from Netlify Function

--- src/views/AddNewTrip.vue
+++ src/views/AddNewTrip.vue
@@ -169,8 +169,9 @@ export default {
         .get(
           `https://dolphin-travel.netlify.app/.netlify/functions/locationSearch?q=${this.hotelName}&countrycodes=${this.country.code}`
         )
-        .then(({ msg }) => {
-          this.accommodations = msg;
+        .then((res) => {
+          this.accommodations = res.data.msg;
+          // console.log(res.data.msg)
         })
         .catch((err) => {
           if (err.code === "ERR_BAD_REQUEST") {
